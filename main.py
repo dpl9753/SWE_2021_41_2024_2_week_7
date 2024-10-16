ad61a4cb86f6077ad72b60cbae08b95c38cd6b0b
KIM DONGHO <dpl9753@naver.com>
2024-10-16 18:56:10 +0900

    edit ìcomment

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def twoSum(nums: List[int], target: int) -> List[int]:
     for i in range(length): #모든 nums에 있는 값에 대해 
         for j in range(i+1, length): #현재 선택한 i의 뒷쪽만 확인
             if nums[i] + nums[j] == target: #두 숫자의 합이 target과 같으면
-                return [i, j] #return
+                return [i, j] #return index
 
 
 print(twoSum([2, 7, 11, 15], 9))
